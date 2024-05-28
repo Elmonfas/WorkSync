8e036afb015678394b662b250503a28cdeeb285b
Monfas <pedromonfort075@gmail.com>
2024-05-28 17:04:06 +0200

    Creacion de el key_token que se guarda en una cookie segura e implementacion del logout el cual elimina la sesuin 'el key_token' que se guarda en la cookie

--- Backend/helpers/add_user.js
+++ Backend/helpers/add_user.js
@@ -12,13 +12,9 @@ async function add_user(user){
 
             await con.query('insert into Users (nombre, apellido, email, password) values (?, ?, ?, ?)', [name, surname, email, password])
 
-            // console.log('El usuario : '+ name + ' ha sido añadido')
-
             return true
 
         }else {
-
-            // console.log('El usuario que intentas añadir ya existe.')
 
             return false
 
